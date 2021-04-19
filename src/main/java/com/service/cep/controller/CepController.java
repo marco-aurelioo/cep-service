f0d7727fe8d67c9c21b66d8ea45cb2ac41f98dad
Tiozao <marco_aurelioo@yahoo.com.br>
2021-04-18 21:44:54 -0300

    teste input controller

--- src/main/java/com/service/cep/controller/CepController.java
+++ src/main/java/com/service/cep/controller/CepController.java
@@ -1,16 +1,27 @@
 package com.service.cep.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 
 @RestController
+@Validated
 public class CepController {
 
     @GetMapping("/cep/{cep}")
-    public ResponseEntity<String> findCep(@PathParam("cep")String cep){
+    public ResponseEntity<String> findCep(@Valid @PathVariable("cep") @Pattern( regexp = "^\\d{8}$") String cep){
         return ResponseEntity.ok(cep);
     }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+        return new ResponseEntity<>("invalid request: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
 }
---
+++ src/test/java/com/service/cep/controller/CepControllerTest.java
@@ -0,0 +1,40 @@
+package com.service.cep.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = CepController.class)
+class CepControllerTest {
+
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void invalidRequest() throws Exception {
+        String uri = "/cep/123456789";
+        mvc.perform(MockMvcRequestBuilders.get(uri))
+                .andExpect(status().isBadRequest());
+    }
+
+
+
+
+
+
+}
